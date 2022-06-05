898713edb6210c6a9adc9970ccf7d482413100d2
powy-e <edfnazario@gmail.com>
2022-06-06 00:24:11 +0100

    Exercicio 2 semi Feito

---
+++ Ex02/2.R
@@ -0,0 +1,38 @@
+library("openxlsx")
+library("ggplot2")
+library("dplyr")
+library("reshape2")
+library("tibble")
+library("stringr")
+library("data.table")
+
+paises <- c("UK - Reino Unido", "ES - Espanha", "FI - Finlândia")
+paisesProcessados<- unlist(lapply(paises, str_replace_all," ","."))
+
+
+dadosHomens <- read.xlsx("./Ex02/EsperancaVida.xlsx",
+    rows = c(9:70), cols = c(1,36:69))  %>%  select( (paisesProcessados) | X1) %>% 
+  filter(X1 %in% 2002:2019)
+colnames(dadosHomens) <- c("UK - M", "ES - M", "FI - M", "Ano")
+dadosHomens <- melt(dadosHomens,id = c("Ano"))
+
+
+
+
+
+
+dadosMulheres <- read.xlsx("./Ex02/EsperancaVida.xlsx",
+  rows = c(9:70), cols = c(1,70:104)) %>%  select((paisesProcessados) | X1) %>% 
+  filter(X1 %in% 2002:2019)
+
+colnames(dadosMulheres) <- c("UK - F", "ES - F", "FI - F", "Ano")
+
+dadosMulheres <- melt(dadosMulheres,id = c("Ano"))
+
+dadosTotais <- rbind(dadosMulheres, dadosHomens) 
+colnames(dadosTotais)<-c("Ano", "Pais", "Valor")
+
+p <- ggplot(dadosTotais, aes(x = Ano, y = Valor, color= Pais)) +
+  geom_line()
+
+  
