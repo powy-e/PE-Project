cdc68a356772a2c9c5061ff1cd83e64289728b4c
powy-e <edfnazario@gmail.com>
2022-06-10 02:37:45 +0100

    Ex04 - Added a label

--- Ex04/4.R
+++ Ex04/4.R
@@ -12,4 +12,6 @@ dados <- read.xlsx("./Ex04/Utentes.xlsx") %>%  select(variaveis)
 
 
 p <- ggplot(dados, aes(x = Colesterol, y= TAD)) +
-  geom_point(color= 'green')
+  geom_point(color= 'black', fill='orange', shape= 21) +
+  labs(title = "Gráfico de Dispersão de TAD e Colestrol") 
+p
