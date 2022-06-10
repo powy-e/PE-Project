0f397d106d3aa5b0d49b18fc721d159f9f4447ab
powy-e <edfnazario@gmail.com>
2022-06-10 19:18:19 +0100

    Changed code Language to PT and added the plot image

--- Ex01/1.R
+++ Ex01/1.R
@@ -7,31 +7,31 @@ library("reshape2")
 
 countries <- c("GR - Grécia", "HR - Croácia", "SI - Eslovénia")
 
-# Read Data from EXCEL and Filter it by the countries we're looking for
-readData <- read.xlsx("./Ex01/ResiduosPerCapita.xlsx",
+# Ler os dados do Excel
+dadosExcel <- read.xlsx("./Ex01/ResiduosPerCapita.xlsx",
     rows = c(12:43), cols = c(1:3)
 )
 
-# Rename columns to clean up the data
-colnames(readData) <- c("Country", "2004", "2018")
+# Renomear as colunas
+colnames(dadosExcel) <- c("País", "2004", "2018")
 
-# Melt the data to get the data in the right format
-readData <- readData %>%
-    filter(Country %in% countries) %>%
-    melt(id = c("Country"))
+# Filtrar os Dados e preparar os dados para fazer o gráfico 
+dadosExcel <- dadosExcel %>%
+    filter(País %in% countries) %>%
+    melt(id = c("País"))
 
-# Name columns
-colnames(readData) <- c("Country", "Year", "Residues")
+# Voltar a renomear colunas
+colnames(dadosExcel) <- c("País", "Ano", "Resíduos")
 
-# Make the plot
-p <- ggplot(readData, aes(x = Country, y = Residues, fill = Year)) +
+# Fazer o Gráfico de Barras
+p <- ggplot(dadosExcel, aes(x = País, y = Resíduos, fill = Ano)) +
     geom_bar(stat = "Identity", position = position_dodge()) +
-    geom_text(aes(label = Residues),
+    geom_text(aes(label = Resíduos),
         vjust = 2.5, size = 4, color = "white",
         position = position_dodge(0.9)
     ) +
-    labs(title="Produced Residues per capita") +
-    ylab("Residues (t)") +
+    labs(title=" Produção de resíduos per capita") +
+    ylab("Resíduos (t)") +
     scale_fill_brewer(palette = "Paired") +
     theme_minimal()
 
