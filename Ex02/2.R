0f500e0387e2e487fe850728b2d25e0c8224f1e7
powy-e <edfnazario@gmail.com>
2022-06-11 02:57:07 +0100

    Changed code to match pdf

--- Ex02/2.R
+++ Ex02/2.R
@@ -3,35 +3,30 @@ library("ggplot2")
 library("dplyr")
 library("reshape2")
 library("stringr")
-library("data.table")
 
 paises <- c("UK - Reino Unido", "ES - Espanha", "FI - Finlândia")
 paisesProcessados<- unlist(lapply(paises, str_replace_all," ","."))
 
-
+# Ler e processar dados das Homens
 dadosHomens <- read.xlsx("./Ex02/EsperancaVida.xlsx",
     rows = c(9:70), cols = c(1,36:69))  %>%  select( (paisesProcessados) | X1) %>% 
   filter(X1 %in% 2002:2019)
 colnames(dadosHomens) <- c("UK - M", "ES - M", "FI - M", "Ano")
 dadosHomens <- melt(dadosHomens,id = c("Ano"))
 
-
+# Ler e processar dados das Mulheres
 dadosMulheres <- read.xlsx("./Ex02/EsperancaVida.xlsx",
   rows = c(9:70), cols = c(1,70:104)) %>%  select((paisesProcessados) | X1) %>% 
   filter(X1 %in% 2002:2019)
-
 colnames(dadosMulheres) <- c("UK - F", "ES - F", "FI - F", "Ano")
-
 dadosMulheres <- melt(dadosMulheres,id = c("Ano"))
 
-
+# Retirar dados inexistentes
 dadosTotais <- rbind(dadosMulheres, dadosHomens) %>% filter(value != 0)
 colnames(dadosTotais)<-c("Ano", "Pais", "EMV")
 
-p <- ggplot(dadosTotais, aes(x = Ano, y = EMV, color= Pais)) +
+# Gráfico
+ggplot(dadosTotais, aes(x = Ano, y = EMV, color= Pais)) +
   geom_line() +
-  theme_light()
-
-  
-
-
+  labs(title=" Evolução da Esperança Média de Vida", subtitle = " No UK, ES, FI por sexo") + ylab("Esperança Média de Vida")+ 
+  theme(plot.subtitle=element_text(size=10, hjust=0.03, color="#808080"))
