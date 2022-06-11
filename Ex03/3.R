e7cbeb9d5c82aa9e115e5af4292074001d5569d2
powy-e <edfnazario@gmail.com>
2022-06-11 04:10:19 +0100

    Ex03 - Made graph with superscript

--- Ex03/3.R
+++ Ex03/3.R
@@ -2,17 +2,21 @@ library("openxlsx")
 library("ggplot2")
 library("dplyr")
 library("reshape2")
-library("stringr")
-library("data.table")
+
 
 regioes <- c("Entrecampos", "VNTelha-Maia")
 
-
+# Ler e selecionar os dados
 dados <- read.xlsx("./Ex03/QualidadeArO3.xlsx") %>%  select(regioes) %>% melt(measure = regioes)
-colnames(dados) <- c("Regiao", "Valor")
+colnames(dados) <- c("Região", "Valor")
 
+# Converter o tipo de dados para Numérico
 dados$Valor <- as.numeric(dados$Valor)
 
-p <- ggplot(dados, aes(x = Valor, fill= Regiao, fill= Regiao)) +
-  geom_histogram(binwidth = 20 , alpha=0.5)
-p
+#Gráfico
+ggplot(dados, aes(x = Valor, fill= Região)) +
+  geom_histogram(bins = 20, alpha=0.7) + 
+  labs(title=" Observações horárias de níveis de ozono",
+       subtitle = expression(" Em Entrecampos e VNTelha-Maia"~µg/m^3), y="Número de Observações",
+       x = expression("Quantidade em "~ µg/m^3)) +
+  theme(plot.subtitle=element_text(size=10, hjust=0.03, color="#808080"))
