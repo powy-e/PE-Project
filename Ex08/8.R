7fc5effd5ac002f63ecef16b764e0469ce0ebd52
powy-e <edfnazario@gmail.com>
2022-06-11 18:51:44 +0100

    Ex08 - Fixed Confidence Interval Formula

--- Ex08/8.R
+++ Ex08/8.R
@@ -1,15 +1,21 @@
 
-set.seed(313)
+set.seed(520)
 
 n_amostras <- 1100
 
 dimensao <- 673
 
+lambda <- 0.78
+
 nivel_confianca <- 0.93
+alpha <- (1-nivel_confianca)
+
+qnt_dnorm <- qnorm(1-alpha/2)
 
 amostras<- c()
 for (i in 1:n_amostras) {
-  amostras[i]<- 2*(sd(rexp(dimensao, 0.78)) * nivel_confianca / sqrt(dimensao))
+  amostra <- rexp(dimensao, lambda)
+  amostras[i] <- 2* qnt_dnorm / sqrt(dimensao) / mean(amostra)
 }
 
 print(mean(amostras))
