d8533342b66d9a025498ad4d7f0bfc2d0bd33b7d
powy-e <edfnazario@gmail.com>
2022-06-11 19:07:11 +0100

    Ex09 - Changed Confidence Level Formula

--- Ex09/9.R
+++ Ex09/9.R
@@ -6,18 +6,23 @@ n_amostras <- 900
 # dimensao das amostras varia entre [100, 5.000] em incrementos de 100
 # 100n , n[1:50]
 
+lambda <- 0.7
 
 nivel_confianca <- 0.99
+alpha <- (1-nivel_confianca)
+
+qnt_dnorm <- qnorm(1-alpha/2)
 
 mediaAmostrasN <- c()
 valor_n <- c()
-
 for (j in 1:50) {
   amostrasN <- c()
+  dimensao <- 100*j
   for (i in 1:n_amostras) {
-      amostrasN[i]<- 2*(sd(rexp(100*j, 0.7)) * nivel_confianca / sqrt(100*j))
+    amostra <- rexp(dimensao, lambda)
+    amostrasN[i]<- 2* qnt_dnorm / sqrt(dimensao) / mean(amostra)
   }
-  valor_n[j] = (100*j)
+  valor_n[j] = dimensao
   mediaAmostrasN[j] <- mean(amostrasN)
 }
 
