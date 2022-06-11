fb38c24faf83d29b553b0c8fda3694dffafcbc35
powy-e <edfnazario@gmail.com>
2022-06-11 19:34:57 +0100

    Ex10 - Changed Confidence Level Formula and fixed a bug

--- Ex10/10.R
+++ Ex10/10.R
@@ -1,13 +1,20 @@
 
-set.seed(835)
+set.seed(484)
 
-n_amostras <- 750
+n_amostras <- 550
 
 # dimensao das amostras varia entre [100, 2.500] em incrementos de 100
 # 100n , n[1:25]
 
-nivel_confianca <- 0.93
 contaminar <- 0.25
+
+lambda <- 0.28
+lambda_c <- 0.17
+  
+nivel_confianca <- 0.92
+alpha <- (1-nivel_confianca)
+
+qnt_dnorm <- qnorm(1-alpha/2)
 
 mediaAmostrasN <- c()
 mediaAmostrasNC <- c()
@@ -16,14 +23,15 @@ valor_n <- c()
 for (j in 1:25) {
   amostrasN <- c()
   amostrasNC <- c()
+  dimensao <- 100*j
   for (i in 1:n_amostras) {
-    descontaminados <- rexp(100*j, 0.35)
-    contaminados <- rexp(100*j*contaminar, 0.02)
-    amostraContaminada <- append(contaminados, descontaminados[(100*j*contaminar + 1):(100*j)])
-    amostrasN[i]<- 2*(sd(descontaminados) * nivel_confianca / sqrt(100*j))
-    amostrasNC[i]<- 2*(sd(amostraContaminada) * nivel_confianca / sqrt(100*j))
+    descontaminados <- rexp(dimensao, lambda)
+    contaminados <- rexp(dimensao*contaminar, lambda_c)
+    amostraContaminada <- append(contaminados, descontaminados[(dimensao*contaminar + 1):(dimensao)])
+    amostrasN[i]<- 2* qnt_dnorm / sqrt(dimensao) / mean(descontaminados)
+    amostrasNC[i]<- 2* qnt_dnorm / sqrt(dimensao) / mean(amostraContaminada)
   }
-  valor_n[j] = (100*j)
+  valor_n[j] = dimensao
   mediaAmostrasN[j] <- mean(amostrasN)
   mediaAmostrasNC[j] <- mean(amostrasNC)
 }
