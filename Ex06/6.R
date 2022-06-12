289ebb1bf7ffa0751f172fce18f210b68a3c91d0
powy-e <edfnazario@gmail.com>
2022-06-12 22:14:52 +0100

    Ex06 - Code comments

--- Ex06/6.R
+++ Ex06/6.R
@@ -1,7 +1,7 @@
 library("ggplot2")
 library("patchwork")
 
-
+#Parâmetros
 inicio <- 4; fim <- 8
 
 n_value <- c(2, 25, 59)
@@ -12,6 +12,7 @@ pop <- 290
 var_unif_distr <- ((fim-inicio)**2)/12
 val_esperado <- mean(c(fim,inicio))
 
+# Geração amostras
 dados <- matrix(nrow = pop, ncol = 3)
 for (n in 1:3) {
   set.seed(seed)
@@ -20,14 +21,13 @@ for (n in 1:3) {
   }
 }
 
-dados <- data.frame(dados)
+dados <- data.frame(dados) # Passar a DataFrame
 
-gráficos <- list()
+gráficos <- list()    # Gráficos
 for (i in 1:3) {
 gráficos[[i]] <-  ggplot(dados, aes_string(colnames(dados)[i])) + geom_histogram(aes(y = stat(count/sum(count))),colour="purple", fill="#FF00FF", alpha = 0.1, bins = 30) +
   xlim(inicio, fim) + stat_function(fun = dnorm, args = list(mean = val_esperado, sd = (sqrt(var_unif_distr / n_value[i]))), color = "black") + 
   labs(title=paste0("Distribuição da Média (n=", n_value[i], ")"), x="Valores da Distribuição da Média", y="Frequência Relativa")
 }
-
 
 Reduce("+", gráficos) 
