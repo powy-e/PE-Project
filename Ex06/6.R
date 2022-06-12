868afe6353e4790a82504b3de5e716e3549c55cf
powy-e <edfnazario@gmail.com>
2022-06-12 21:35:41 +0100

    Ex06 - Graph and Code

--- Ex06/6.R
+++ Ex06/6.R
@@ -4,50 +4,30 @@ library("patchwork")
 
 inicio <- 4; fim <- 8
 
-n1 <- 2; n2 <- 25; n3 <- 59
+n_value <- c(2, 25, 59)
+seed <- 1681
 
 pop <- 290
 
-dadosN1<- c()
-dadosN2<- c()
-dadosN3<- c()
+var_unif_distr <- ((fim-inicio)**2)/12
+val_esperado <- mean(c(fim,inicio))
 
-set.seed(1681)
-for(i in 1:pop) { 
-  dadosN1[i] <- mean(runif(n1, inicio, fim)) 
-}
-set.seed(1681)
-for(i in 1:pop) { 
-  dadosN2[i] <- mean(runif(n2, inicio, fim)) 
-}
-set.seed(1681)
-for(i in 1:pop) { 
-  dadosN3[i] <- mean(runif(n3, inicio, fim)) 
+dados <- matrix(nrow = pop, ncol = 3)
+for (n in 1:3) {
+  set.seed(seed)
+  for(i in 1:pop) { 
+    dados[i,n] <- mean(runif(n_value[n], inicio, fim)) 
+  }
 }
 
+dados <- data.frame(dados)
 
-dados <- data.frame(dadosN1, dadosN2, dadosN3)
-dados$dadosN1 <-  as.numeric(dados$dadosN1)
-
-p <- ggplot(dados, aes(x = Valor, fill= Regiao)) +
-  geom_histogram(binwidth = 10 , alpha=0.5)
-
-
-p1 <- ggplot(dados, aes(dadosN1)) + geom_histogram(aes(y = after_stat(count/sum(count))),colour="black", fill="white") +
-  xlim(inicio, fim) +
-  stat_function(fun = dnorm, args = list(mean = (mean(dados$dadosN1)),
-                                         sd = (sd(dadosN1) / sqrt((n1)))))
-
-p2 <- ggplot(dados, aes(dadosN2)) + geom_histogram(aes(y = after_stat(count/sum(count))),colour="black", fill="white") +
-  xlim(inicio, fim)  +
-  stat_function(fun = dnorm, args = list(mean = (mean(dados$dadosN2)),
-                                         sd = (sd(dadosN2) / sqrt((n2)))))
-
-p3 <- ggplot(dados, aes(dadosN3)) + geom_histogram(aes(y = after_stat(count/sum(count))),colour="black", fill="white") +
-  xlim(inicio, fim) +
-  stat_function(fun = dnorm, args = list(mean = (mean(dados$dadosN3)),
-                                         sd = (sd(dadosN3) / sqrt((n3)))))
-
-p1+p2+p3
+gráficos <- list()
+for (i in 1:3) {
+gráficos[[i]] <-  ggplot(dados, aes_string(colnames(dados)[i])) + geom_histogram(aes(y = stat(count/sum(count))),colour="purple", fill="#FF00FF", alpha = 0.1, bins = 30) +
+  xlim(inicio, fim) + stat_function(fun = dnorm, args = list(mean = val_esperado, sd = (sqrt(var_unif_distr / n_value[i]))), color = "black") + 
+  labs(title=paste0("Distribuição da Média (n=", n_value[i], ")"), x="Valores da Distribuição da Média", y="Frequência Relativa")
+}
 
 
+Reduce("+", gráficos) 
