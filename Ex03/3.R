39a8b992f098f8c6be1333c67799e77655055207
powy-e <edfnazario@gmail.com>
2022-06-12 02:22:00 +0100

    Ex03 - Changed and Re-rendered Graph

--- Ex03/3.R
+++ Ex03/3.R
@@ -15,9 +15,11 @@ dados$Valor <- as.numeric(dados$Valor)
 
 #Gráfico
 ggplot(dados, aes(x = Valor, color= Região, fill = Região)) +
-  geom_histogram(binwidth = 15, alpha=0.5, position="Identity") + 
+  geom_histogram(binwidth = 10, alpha=0.5, position="Identity") + 
   labs(title=" Observações horárias de níveis de ozono",
        subtitle = expression(" Em Entrecampos e VNTelha-Maia"~µg/m^3), y="Número de Observações",
        x = expression("Quantidade em "~ µg/m^3)) +
   theme(plot.subtitle=element_text(size=10, hjust=0.03, color="#808080")) + 
-  scale_color_brewer(palette = "Set1")
+  scale_color_brewer(palette = "Set1") +
+  scale_x_continuous(breaks = seq(0, 165, 15))
+  
