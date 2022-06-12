a0d2d15161950e4b9e626513108344acb1e10e37
powy-e <edfnazario@gmail.com>
2022-06-12 02:27:05 +0100

    Ex04 - Comments and graph image

--- Ex04/4.R
+++ Ex04/4.R
@@ -1,9 +1,6 @@
 library("openxlsx")
 library("ggplot2")
 library("dplyr")
-library("reshape2")
-library("stringr")
-library("data.table")
 
 variaveis <- c("Colesterol", "TAD")
 
@@ -11,7 +8,7 @@ variaveis <- c("Colesterol", "TAD")
 dados <- read.xlsx("./Ex04/Utentes.xlsx") %>%  select(variaveis)
 
 
-p <- ggplot(dados, aes(x = Colesterol, y= TAD)) +
-  geom_point(color= 'black', fill='orange', shape= 21) +
-  labs(title = "Gráfico de Dispersão de TAD e Colestrol") 
-p
+ggplot(dados, aes(x = Colesterol, y= TAD)) +
+  geom_point(color= 'black', fill='orange', shape= 21) + xlim(0,320) + ylim(0, 110) +
+  labs(title = "Gráfico de Dispersão de TAD e Colestrol", subtitle = "em 76 Utentes") + geom_smooth(method = "lm", se =FALSE) +
+  theme(plot.subtitle=element_text(size=10, hjust=0.03, color="#808080"))
