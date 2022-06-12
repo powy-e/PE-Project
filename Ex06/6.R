28dea7682be3bb88b2665cc20253f7fdbb2ff098
powy-e <edfnazario@gmail.com>
2022-06-12 19:10:44 +0100

    Ex06 - set seed

--- Ex06/6.R
+++ Ex06/6.R
@@ -1,12 +1,10 @@
+library("ggplot2")
+library("patchwork")
 
-set.seed(1681)
 
-inicio <- 4
-fim <- 8
+inicio <- 4; fim <- 8
 
-n1 <- 2
-n2 <- 25
-n3 <- 59
+n1 <- 2; n2 <- 25; n3 <- 59
 
 pop <- 290
 
@@ -14,35 +12,42 @@ dadosN1<- c()
 dadosN2<- c()
 dadosN3<- c()
 
+set.seed(1681)
 for(i in 1:pop) { 
   dadosN1[i] <- mean(runif(n1, inicio, fim)) 
 }
-
+set.seed(1681)
 for(i in 1:pop) { 
   dadosN2[i] <- mean(runif(n2, inicio, fim)) 
 }
-
+set.seed(1681)
 for(i in 1:pop) { 
   dadosN3[i] <- mean(runif(n3, inicio, fim)) 
 }
 
 
-media1 <- mean(unif1)
-media2 <- mean(unif2)
-media3 <- mean(unif3)
+dados <- data.frame(dadosN1, dadosN2, dadosN3)
+dados$dadosN1 <-  as.numeric(dados$dadosN1)
 
 p <- ggplot(dados, aes(x = Valor, fill= Regiao)) +
   geom_histogram(binwidth = 10 , alpha=0.5)
 
 
+p1 <- ggplot(dados, aes(dadosN1)) + geom_histogram(aes(y = after_stat(count/sum(count))),colour="black", fill="white") +
+  xlim(inicio, fim) +
+  stat_function(fun = dnorm, args = list(mean = (mean(dados$dadosN1)),
+                                         sd = (sd(dadosN1) / sqrt((n1)))))
 
-ggplot() + aes(dadosN1)+ geom_histogram(aes(y = after_stat(count / sum(count))),colour="black", fill="white") +
-  xlim(inicio, fim) + scale_y_continuous(labels = scales::percent) +
-  stat_function(fun = dnorm, args = list((min(dadosN1)+max(dadosN1))/2,  (sd(dadosN1)/sqrt(n1))))
+p2 <- ggplot(dados, aes(dadosN2)) + geom_histogram(aes(y = after_stat(count/sum(count))),colour="black", fill="white") +
+  xlim(inicio, fim)  +
+  stat_function(fun = dnorm, args = list(mean = (mean(dados$dadosN2)),
+                                         sd = (sd(dadosN2) / sqrt((n2)))))
 
-ggplot() + aes(dadosN2)+ geom_histogram(aes(y = after_stat(count / sum(count))),colour="black", fill="white") + 
-  xlim(inicio, fim) + scale_y_continuous(labels = scales::percent)
+p3 <- ggplot(dados, aes(dadosN3)) + geom_histogram(aes(y = after_stat(count/sum(count))),colour="black", fill="white") +
+  xlim(inicio, fim) +
+  stat_function(fun = dnorm, args = list(mean = (mean(dados$dadosN3)),
+                                         sd = (sd(dadosN3) / sqrt((n3)))))
 
-ggplot() + aes(dadosN3)+ geom_histogram(aes(y = after_stat(count / sum(count))),colour="black", fill="white") + 
-  xlim(inicio, fim) + scale_y_continuous(labels = scales::percent)
+p1+p2+p3
+
 
